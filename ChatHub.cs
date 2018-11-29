b988e6e06c83318c7aa12ea994078ebfa167217b
WillB01 <william.hello@outlook.com>
2018-11-29 14:29:41 +0100

    first commit

---
+++ ChatHub.cs
@@ -0,0 +1,29 @@
+﻿using LinkedInSignalR.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinkedInSignalR
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string name, string text)
+        {
+            var message = new ChatMessage
+            {
+                SenderName = name,
+                Text = text,
+                SentAt = DateTimeOffset.UtcNow
+            };
+
+            // Broadcast to all clients
+            await Clients.All.SendAsync(
+                "ReceiveMessage", 
+                message.SenderName, 
+                message.SentAt, 
+                message.Text);
+        }
+    }
+}
---
+++ wwwroot/js/Index.js
@@ -0,0 +1,53 @@
+﻿const chatterName = 'Visitor0';
+
+// Initialize the Signlar cline
+
+var connection = new signalR.HubConnectionBuilder()
+    .withUrl('/chatHub')
+    .build();
+
+connection.on('ReceiveMessage', renderMessage);
+connection.start();
+
+document.addEventListener('DOMContentLoaded', () => {
+    ready();
+});
+
+function ready() {
+    //setTimeout(showChatDialog, 750);
+    
+    const chatFormEl = document.querySelector('#chatForm');
+    chatFormEl.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let text = e.target[0].value;
+        e.target[0].value = '';
+        sendMessage(text);
+
+
+    });
+
+};
+
+
+function renderMessage(name, time, message) {
+    console.log(`${name} ${time} ${message}`);
+    var chatBox = document.querySelector('#chatBox');
+    chatBox.innerHTML = `${name} : ${time}
+                        ${message}`;
+};
+
+function showChatDialog() {
+    let dialogEl = document.querySelector('#chatDialog');
+    dialogEl.style.display = 'block';
+
+};
+
+
+
+function sendMessage(text) {
+    if (text && text.length) {
+        connection.invoke('SendMessage', chatterName, text);
+    }
+};
+
+
